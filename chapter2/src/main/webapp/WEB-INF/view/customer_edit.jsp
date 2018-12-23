9de965485cd98047a11960b9ba4cfe34a5482db9
Ray <qianlei6148@163.com>
2018-12-23 20:43:44 +0800

    init

---
+++ chapter2/src/main/webapp/WEB-INF/view/customer_edit.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shenyi
+  Date: 2018/12/23
+  Time: 下午8:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>修改客户</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ chapter2/src/test/java/io/github/qianlei/chapter2/test/CustomerServiceTest.java
@@ -0,0 +1,63 @@
+package io.github.qianlei.chapter2.test;
+
+import io.github.qianlei.chapter2.model.Customer;
+import io.github.qianlei.chapter2.service.CustomerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceTest {
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        customerService = new CustomerService();
+    }
+
+    @Before
+    public void init() {
+        //TODO
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception {
+        List<Customer> customerList = customerService.getCustomerList("");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() throws Exception {
+        long id=1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone", "12345678912");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+}
